77a39c3427c177ff12cc6dca439cb179a464ecf6
Caioalvesmartinsdesouza <128051699+Caioalvesmartinsdesouza@users.noreply.github.com>
2023-09-15 08:15:29 -0300

    Add files via upload

--- Caio/Teste 1.c
+++ Caio/Teste 1.c
@@ -1,13 +1,27 @@
 #include<stdio.h>
 #include<stdlib.h>
+void funcao() {
+printf("Funcao funciona\n");
+}
 
 int main(){
-int num1;
-float num2;
+int num1, num2;
+float num3;
 char letra1;
-char palavra1[] = "Palavra";
-printf("Digite o valor:\n");
+char palavra1[21];
+printf("Digite um valor maior ou igual a 5:\n");
 scanf("%d", &num1);
-printf("%d\n", num1);
+if(num1>=5){
+printf("Digite a palavra:\n");
+scanf("%s", palavra1);
+for (num2=1;num2<4;num2++){
+printf("O valor e: %d\n", num1);
+printf("A palavra e: %s\n", palavra1);
+funcao();
+}
+}
+else{
+printf("Voce digitou um valor menor que cinco\n");	
+}
 system("pause");
 }
