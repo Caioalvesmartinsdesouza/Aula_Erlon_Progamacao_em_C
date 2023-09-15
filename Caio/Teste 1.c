ce5dae26a9ddac3ea658f75fcb21a85cb89497b8
Caioalvesmartinsdesouza <128051699+Caioalvesmartinsdesouza@users.noreply.github.com>
2023-09-15 07:57:09 -0300

    Add files via upload

--- Caio/Teste 1.c
+++ Caio/Teste 1.c
@@ -2,6 +2,12 @@
 #include<stdlib.h>
 
 int main(){
-printf("Hello World\n");
+int num1;
+float num2;
+char letra1;
+char palavra1[] = "Palavra";
+printf("Digite o valor:\n");
+scanf("%d", &num1);
+printf("%d\n", num1);
 system("pause");
 }
